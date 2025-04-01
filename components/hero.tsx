71ed84bed10e54ab4c9a666a642da5ff88e28af3
Vinh B <vinh.bui0101@gmail.com>
2025-04-01 16:41:31 -0700

    added smooth motion for logo

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,17 @@
+"use client"
+import { motion } from 'framer-motion';
+
 const Hero = () => {
   return (
     <div className="w-full h-screen bg-cover bg-center flex justify-center items-center">
-      <img src="/logoInBlue.png" alt="Logo" className="w-1/5" />
+      <motion.img 
+        src="/logoInBlue.png" 
+        alt="Logo" 
+        className="w-1/5" 
+        initial={{ opacity: 0, scale: 0.8}}
+        animate={{ opacity: 1, scale: 1}}
+        transition={{ duration: 2, delay: 0.5 }}
+      />
     </div>
   );
 };
