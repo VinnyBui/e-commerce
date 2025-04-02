1954d318ca2d512f5c3149062d7dd8792b50d90b
Vinh B <vinh.bui0101@gmail.com>
2025-04-01 17:01:21 -0700

    added text and animations to hero page

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,16 +3,29 @@ import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-cover bg-center flex justify-center items-center">
+    <section className="w-full h-screen flex justify-center items-center relative">
+      {/* Text in the top left */}
+      <div className="absolute top-10 left-10 text-primary font-semibold">
+        <motion.p 
+          className="text-[0.8rem] md:text-lg"
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{ duration: 2, delay: 0.5 }}       
+        >
+          OPTIMIZING PRODUCTIVITY, ONE STEP AT A TIME
+        </motion.p>
+      </div>
+
+      {/* Logo in the center */}
       <motion.img 
         src="/logoInBlue.png" 
         alt="Logo" 
-        className="w-1/5" 
-        initial={{ opacity: 0, scale: 0.8}}
-        animate={{ opacity: 1, scale: 1}}
+        className=" w-3/4 md:w-1/5"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 2, delay: 0.5 }}
       />
-    </div>
+    </section>
   );
 };
 
